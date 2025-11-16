64caa03e3e068080ae8ec70cad75b3cbd798f454
Pramod_Bhavikhatte <bhavikhattepramod@gmail.com>
2025-11-16 20:31:04 +0530

    His Script

---
+++ Individuals/individual.py
@@ -0,0 +1,42 @@
+import xmlrpc.client
+import pandas as pd
+
+# Odoo connection details
+
+url = 'http://localhost:1717'
+db = 'his_09112025'
+user = 'kaja@blackbadger.biz'
+password = 'kaja@blackbadger.biz'
+
+
+common = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/common', allow_none=True)
+uid = common.authenticate(db, user, password, {})
+models = xmlrpc.client.ServerProxy(f'{url}/xmlrpc/2/object', allow_none=True)
+
+# File path
+file_path = "individuals.csv"
+df = pd.read_csv(file_path)
+df = df.head(2)
+
+def convert_value(val):
+    if pd.isna(val):
+        return ""  # Keep empty string if null
+    if isinstance(val, pd.Timestamp):
+        return val.strftime("%Y-%m-%d")  # Convert dates to string
+    return str(val)
+
+for col in df.columns:
+    df[col] = df[col].map(convert_value)
+
+records = df.to_dict(orient="records")
+
+try:
+    result = models.execute_kw(
+        db, uid, password,
+        'res.partner', 'import_bulk_individuals',
+        [records]
+    )
+except Exception as e:
+    result = [{'error': str(e)} for _ in records]
+
+print(result)
